888c82e0baa15452fdfe5fa5fd299aeaad8b44ab
KeithNull <s101315@gmail.com>
2024-04-09 12:08:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ write_query = create_sql_query_chain(llm, db)
 
 
 template = """
-You are a database specialist expert. Given an input question, first create a syntactically correct sql query to run, then look at the results of the query and return the answer to the input question.
+You are a sql specialist expert. Given an input question, first create a syntactically correct sql query to run, then look at the results of the query and return the answer to the input question.
 Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per sql. You can order the results to return the most informative data in the database.
 Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
 Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
